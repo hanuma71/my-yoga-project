f98f36ddfb5306c9edc3b1b153de8648ffc0358a
irina <hanuma71@gmail.com>
2020-05-06 18:01:53 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ let tab = document.querySelectorAll('.info-header-tab'),
     //таймер
     //получает данные для таймера
     //let deadTime = prompt("введите двту в формате хххх-хх-хх");
-    let deadTime = '2020-05-05';
+    let deadTime = '2020-05-08';
     function getTimeRemaining(endTime){
         let t = Date.parse(endTime) - Date.parse(new Date()),
         seconds = Math.floor((t/1000)%60),
@@ -160,4 +160,50 @@ let tab = document.querySelectorAll('.info-header-tab'),
             input[i].value = '';
         }
     });
+
+    //slider
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'), 
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+        showSlides(slideIndex);
+
+        function showSlides(n){
+            if(n>slides.length){
+                slideIndex = 1;
+            }
+            if(n<1){
+                slideIndex = slides.length;
+            }
+            slides.forEach((item)=> item.style.display = "none");
+            dots.forEach((item) => item.classList.remove('dot-active'));
+            slides[slideIndex -1].style.display = "block";
+            dots[slideIndex -1].classList.add('dot-active');
+
+        }
+        function plussSlides(n){
+            showSlides(slideIndex +=n);
+        }
+        function currentSlide(n){
+            showSlides(slideIndex =n);
+        }
+        prev.addEventListener('click', function(){
+            plussSlides(-1);
+
+        });
+        next.addEventListener('click', function(){
+            plussSlides(1);
+
+        });
+        dotsWrap.addEventListener('click', function(event){
+            for (let i = 0; i < dots.length + 1; i++){
+                if(event.target.classList.contains('dot') && event.target == dots[i-1]){
+                    currentSlide(i);
+                }
+            }
+        });
+        
 });
